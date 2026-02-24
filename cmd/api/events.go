64bde2c1de1ab3dd40d1bc68a86501f4ef3de016
Nasib Mammadov <bduti09@gmail.com>
2026-02-24 17:19:00 +0400

    40:35

---
+++ cmd/api/events.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"rest-api-in-gin/internal/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) createEvent(c *gin.Context) {
+	var event database.Event
+
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := app.models.Events.insert(&event)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, event)
+}
+
+func (app *application) getEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+	}
+
+	event, err := app.models.Events.Get(id)
+
+	if event == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+
+	}
+
+	c.JSON(http.StatusOK, event)
+}
+
+func (app *application) updateEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+	}
+
+	existingEvent, err := app.models.Events.Get(id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+	}
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/mr-crodo/rest-api-in-gin/internal/database"
+	"github.com/mr-crodo/rest-api-in-gin/internal/env"
 )
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    database.Models
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./data.db")
@@ -12,4 +24,15 @@ func main() {
 	}
 
 	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123456"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
 }
